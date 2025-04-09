29fea6a53d604bce6c52d8664d4c1ce59d26919d
Ian <beaniehutch@gmail.com>
2025-04-08 21:51:36 -0400

    update again - ian

--- Final_project.py
+++ Final_project.py
@@ -1,3 +1,3 @@
 # Randomization + validation of user input algo - Sebas
 # New idea about shop in-game - can't use user input - Aminata
-# add healthbar - ian
+# add healthbar and healthbar data - ian
